7a6a789d4143ccdcf57fde39b3cae488d782b392
Giampaolo Tranchida <giampaolo@tranchida.ch>
2024-02-10 09:03:31 +0100

    fix word

--- m521/tranchida/Papa.java
+++ m521/tranchida/Papa.java
@@ -6,7 +6,7 @@ public class Papa {
     
     public static void main(String[] args) {
         
-        String word = "charlie";
+        String word = "CHARLIE";
         int rows = 20;
         int cols = 20;
 
